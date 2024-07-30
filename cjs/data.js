7d752e2583a344cc429c31512a59a11d9a1bf666
Ivan <miniglez.98@gmail.com>
2024-07-30 21:24:21 +0200

    Ejercicio Terminado

---
+++ cjs/data.js
@@ -0,0 +1,15 @@
+const dataObj = {
+    title: "TITULO DE PRUEBA PARA PONER EN EL SERVER",
+    subtitle: "Aqui iria un subtitulo pero no se me ocurre nada que poner",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Sed iaculis gravida justo in pharetra. Vestibulum fermentum imperdiet lorem 
+    nec vestibulum. Suspendisse nec consectetur est, id vestibulum arcu. Maecenas 
+    quis nulla ante. Proin eu sapien sed justo aliquam iaculis. Curabitur vel mollis 
+    ante. Morbi eget quam non risus tempus consequat sit amet a ex. Phasellus consectetur 
+    justo sem, nec venenatis risus vehicula eget. Phasellus quis nisl id justo condimentum 
+    mattis. Donec eros libero, suscipit ac commodo nec, tempus non libero. Nulla consequat 
+    ultrices lorem in porttitor. In vel dolor quis odio bibendum dapibus. Duis sit amet scelerisque urna, 
+    sed interdum enim.`
+}
+
+module.exports = dataObj
---
+++ server.js
@@ -0,0 +1,17 @@
+const dataObj = require("./cjs/data.js")
+const http = require("node:http")
+
+const server = http.createServer((req, res) => {
+    console.log("Request sucess")
+    res.end(
+        `
+            <h1>${dataObj.title}</h1>
+            <h2>${dataObj.subtitle}</h2>
+            <p>${dataObj.description}</p>
+        `
+    )
+})
+
+server.listen(0, () => {
+    console.log(`http://localhost:${server.address().port}`)
+})
